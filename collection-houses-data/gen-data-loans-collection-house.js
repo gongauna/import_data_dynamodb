833bbedc34f26547b3dff04847c348ff5eceff9d
Gonzalo Gauna <gonzalo.gauna@vana.gt>
2023-06-28 11:15:03 -0300

    feat: importo collection houses data

---
+++ collection-houses-data/gen-data-loans-collection-house.js
@@ -0,0 +1,135 @@
+const readXlsxFile = require('read-excel-file/node')
+const fs = require('fs');
+const AWS = require('aws-sdk');
+
+
+const getUserLoan = async (loanId) => {
+  AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    sessionToken: process.env.AWS_SESSION_TOKEN,
+    region: 'us-east-1' // Replace with your desired AWS region
+  });
+
+  const dynamodbClient = new AWS.DynamoDB.DocumentClient();
+  const params = {
+    TableName: 'loan',
+    Key: {
+      id: loanId
+    }
+  };
+  const { Item } = await dynamodbClient.get({
+    TableName: 'loan',
+    Key: {
+      'loan_id': loanId
+    },
+  }).promise();
+  return Item["user_id"] ?? "to_be_defined";
+}
+
+function generateCollectionHouseRecords() {
+const schemaLoans = {
+    'bucket_id': {
+      prop: 'bucket_id',
+      type: String
+    },
+    'loan_id': {
+      prop: 'loan_id',
+      type: String
+    },
+    'house_id': {
+      prop: 'house_id',
+      type: String
+    },
+    'assigned_at': {
+      prop: 'assigned_at',
+      type: Date
+    },
+    'assigned_end_at': {
+      prop: 'assigned_end_at',
+      type: Date
+    },
+}
+
+const bucketsToId = {
+  "91-180": "bucket_gt_1"
+}
+
+readXlsxFile('./data-collection-houses_1_credito.xlsx', { schema: schemaLoans, sheet: 'loans'}).then(async (rows) => {
+  const arrayBuckets = rows.rows;
+  
+  const now = new Date();
+  const jsonLoansArray = await Promise.all(arrayBuckets.map(async (row) => {
+    const userId = await getUserLoan(row.loan_id);
+    const assignedAt = new Date(row.assigned_at);
+    const assignedEndAt = new Date(row.assigned_end_at);
+    const bucketIdHandled = bucketsToId[row.bucket_id];
+    return {
+        PutRequest: {
+          Item: {
+            pk: {S:`LOAN|${row.loan_id}`},
+            sk: {S:`${row.house_id}|BUCKET|${bucketIdHandled}`},
+            props: {
+              M: {
+                assigned_at: {
+                  S: assignedAt.toISOString()
+                },
+                assigned_end_at: {
+                  S: assignedEndAt.toISOString()
+                },
+                loan_id: {
+                  S: row.loan_id
+                },
+                user_id: {
+                  S: userId
+                },
+                bucket_name: {
+                  S: `Bucket ${row.bucket_id}`
+                },
+                bucket_id: {
+                  S: bucketIdHandled
+                }            
+              },
+            },
+            created_at: {
+              S: now.toISOString()
+            },
+            updated_at: {
+              S: now.toISOString()
+            },
+            shown_id: {
+              S: row.loan_id
+            },
+            type: {
+              S: "LOAN|HOUSE"
+            },
+            status:{
+              S: "active"
+            }
+          }
+        }
+    };
+  }));
+
+  const cantRequest = 24;
+  const cantFiles = Math.ceil(jsonLoansArray.length / cantRequest);
+
+  for (let r=0; r< cantFiles; r++) {
+    const startRow = r*cantRequest;  
+    const endRow = (r+1)*cantRequest;
+    const filtered = jsonLoansArray.filter((row) => jsonLoansArray.indexOf(row) >= startRow && jsonLoansArray.indexOf(row) < endRow);
+    let collectionHousesRecordsJson = {
+      collection_house_records: filtered
+    };
+    fs.writeFile(`./files_to_import/collection_houses_loans_${r}.json`,JSON.stringify(collectionHousesRecordsJson),"utf8", function (err) {
+        if (err) {
+          console.log("Error"+err);
+        }
+        console.log(`Collection houses loans ${r} JSON file saved`);
+    })
+  }
+});
+}
+
+module.exports.generateCollectionHouseRecords = generateCollectionHouseRecords;
+
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 //User-Role-Permission generation
-const permissions = require('./user-role-permission/gen-data-permisos');
+/*const permissions = require('./user-role-permission/gen-data-permisos');
 const roles = require('./user-role-permission/gen-data-roles');
 const users = require('./user-role-permission/gen-data-users');
-const userRoles =  require('./user-role-permission/gen-data-user-role');
+const userRoles =  require('./user-role-permission/gen-data-user-role');*/
+const collectionHouseRecords = require('./collection-houses-data/gen-data-loans-collection-house');
 
-permissions.generatePermissions();
+/*permissions.generatePermissions();
 roles.generateRoles();
 users.generateUsers();
 userRoles.generateUserRoles();
@@ -12,9 +13,33 @@ userRoles.generateUserRoles();
 //User-Role-Permission generation
 const collection_annotation_types = require('./collection-annotation-types/gen-data-annotation-types');
 
-collection_annotation_types.generateAnnotationTypes();
+collection_annotation_types.generateAnnotationTypes();*/
+
+
+//Collection houses records
+collectionHouseRecords.generateCollectionHouseRecords();
 
 //Docu:
 
 // Get data from cognito:: aws cognito-idp list-users --user-pool-id us-east-1_VOSHOY7Am > usersCognito.json
 //Para importar: aws dynamodb batch-write-item --request-items file://users_dev_0.json
+
+/*aws dynamodb batch-write-item --request-items file://collection_houses_loans_0.json
+aws dynamodb batch-write-item --request-items file://collection_houses_loans_1.json
+aws dynamodb batch-write-item --request-items file://collection_houses_loans_2.json
+aws dynamodb batch-write-item --request-items file://collection_houses_loans_3.json
+aws dynamodb batch-write-item --request-items file://collection_houses_loans_4.json
+aws dynamodb batch-write-item --request-items file://collection_houses_loans_5.json
+aws dynamodb batch-write-item --request-items file://collection_houses_loans_6.json
+aws dynamodb batch-write-item --request-items file://collection_houses_loans_7.json
+aws dynamodb batch-write-item --request-items file://collection_houses_loans_8.json
+aws dynamodb batch-write-item --request-items file://collection_houses_loans_9.json
+aws dynamodb batch-write-item --request-items file://collection_houses_loans_10.json
+aws dynamodb batch-write-item --request-items file://collection_houses_loans_11.json
+aws dynamodb batch-write-item --request-items file://collection_houses_loans_12.json
+aws dynamodb batch-write-item --request-items file://collection_houses_loans_13.json
+aws dynamodb batch-write-item --request-items file://collection_houses_loans_14.json
+aws dynamodb batch-write-item --request-items file://collection_houses_loans_15.json
+aws dynamodb batch-write-item --request-items file://collection_houses_loans_16.json
+aws dynamodb batch-write-item --request-items file://collection_houses_loans_17.json
+aws dynamodb batch-write-item --request-items file://collection_houses_loans_18.json*/
