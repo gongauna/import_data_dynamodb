4a76074341e385640d1724240724b5cd91b64da2
Gonzalo Gauna <gonzalo.gauna@vana.gt>
2024-02-27 10:45:22 -0300

    feat: wip

--- collection-house-create-user-cognito.sh
+++ collection-house-create-user-cognito.sh
@@ -1,7 +1,7 @@
 aws cognito-idp admin-create-user \
   --user-pool-id us-east-1_MZDlf6lho \
   --username jnovales@novales.com.gt \
-  --user-attributes Name=email,Value=jnovales@novales.com.gt Name=name,Value=J Novales
+  --user-attributes Name=email,Value=jnovales@novales.com.gt Name=name,Value="J Novales"
 
 aws cognito-idp admin-set-user-password \
   --user-pool-id us-east-1_MZDlf6lho \
--- collection-house-create-user.js
+++ collection-house-create-user.js
@@ -8,10 +8,3 @@ const lastName = "Novales"
 const email = "jnovales@novales.com.gt"
 
 createUser.createUserUseCase(house, numUser, email, firstName, lastName);
-
-// To Create user cognito
-
-//aws cognito-idp admin-create-user \
-//  --user-pool-id YOUR_USER_POOL_ID \
-//  --username USERNAME \
-//  --user-attributes Name=email,Value=user@example.com Name=phone_number,Value=+1234567890
